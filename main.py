2d1f4d3a3e79aaae1a3cd97d09fef2c57edf00d1
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-09-29 00:57:38 +0530

    add a for loop, add a second condition to check other numbers

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 def finding_prime(num):
-    if num == 2:
-        print(f"{num} is a prime number")
- 
+    for numbers in range(2, num-1):
+        if num == 2:
+            print(f"{num} is a prime number")
+        elif num % numbers == 0:
+            print(f"{num} is not a prime number")
+        
+    
+            
+                
